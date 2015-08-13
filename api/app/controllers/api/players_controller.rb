2c73efea42690bcdba2a7aff96d6b20e1db169ea
Tanner Donovan <tdonovan@sheetmusicplus.com>
2015-08-12 18:34:26 -0700

    update Api::PlayersController to use Foosball gem use cases

--- api/app/controllers/api/players_controller.rb
+++ api/app/controllers/api/players_controller.rb
@@ -1,24 +1,57 @@
 class Api::PlayersController < ApplicationController
   def index
-    @players = player_repo.all
-
-    render json: @players, each_serializer: player_serializer
+    Foosball.present_players(
+      player_repo:  player_repo,
+      observer:     self
+    ) do |o|
+      o.on_success -> (observer, players) {
+        observer.render_players(players)
+      }
+    end
   end
 
   def show
-    @player = player_repo.find(params[:id])
-
-    render json: @player, serializer: player_serializer
+    Foosball.present_player(
+      player_id:    params[:id],
+      player_repo:  player_repo,
+      observer:     self
+    ) do |o|
+      o.on_success -> (observer, player) {
+        observer.render_player(player)
+      }
+    end
   end
 
   def create
-    @player = player_repo.new(player_params)
+    Foosball.create_player(
+      attributes:   player_params,
+      player_repo:  player_repo,
+      observer:     self
+    ) do |o|
+      o.on_success -> (observer, player) {
+        observer.render_player_created(player)
+      }
 
-    if @player.save
-      render json: @player, serializer: player_serializer, status: :created, location: api_player_url(@player)
-    else
-      render json: @player.errors, status: :unprocessable_entity
+      o.on_errors -> (observer, player) {
+        observer.render_player_with_errors(player)
+      }
     end
+  end
+
+  def render_players(players)
+    render json: players, each_serializer: player_serializer
+  end
+
+  def render_player(player)
+    render json: player, serializer: player_serializer
+  end
+
+  def render_player_created(player)
+    render json: player, serializer: player_serializer, status: :created, location: api_player_url(player)
+  end
+
+  def render_player_with_errors(player)
+    render json: player.errors, status: :unprocessable_entity
   end
 
   private
