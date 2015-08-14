6359ecc71e488634b7b9d56ecd4708ddc8c45a41
Tanner Donovan <tdonovan@sheetmusicplus.com>
2015-08-14 09:05:36 -0700

    add ability to print all players to foosball-cli console

--- cli/lib/foosball_cli.rb
+++ cli/lib/foosball_cli.rb
@@ -9,5 +9,15 @@ require 'persistence'
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 
 module FoosballCli
-  # Your code goes here...
+  extend self
+
+  def all_players
+    Foosball.present_players({ player_repo: player_repo })
+  end
+
+  private
+
+  def player_repo
+    Persistence::Repos::PlayerRepo
+  end
 end
--- cli/lib/foosball_cli/console.rb
+++ cli/lib/foosball_cli/console.rb
@@ -1,8 +1,16 @@
 require 'foosball_cli'
 
+def print_all_players
+  _, players = FoosballCli.all_players
+  players.each do |player|
+    puts "#{player.name} \##{player.id}"
+  end
+end
+
 while (print '> '; input = gets) do
   input.chomp!
   case input
+  when 'players' then print_all_players
   when 'time' then puts Time.now
   when 'version' then puts FoosballCli::VERSION
   when 'q', 'quit', 'exit' then break
